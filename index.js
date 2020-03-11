b1be10a6ff8f6c9d856d08284f493d406f6c8119
shinitakute <shinitakute@projectrd.elementfx.com>
2020-03-10 19:31:13 -0700

    fix time centering

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ Number.prototype.pad = function(n) {
     return r;
 };
 
+function getTextWidth(text, font) {
+    var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
+    var context = canvas.getContext("2d");
+    context.font = font;
+    var metrics = context.measureText(text);
+    return metrics.width;
+}
+
 function updateClock() {
     var now = new Date();
     var ms = now.getMilliseconds(),
@@ -13,11 +21,17 @@ function updateClock() {
         day = now.getDate(),
         year = now.getFullYear();
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var viewportWidth = document.documentElement.clientWidth;
+    var viewportHeight = document.documentElement.clientHeight;
+    var fontSize = viewportHeight*30*0.01;
+    var font = "small-caps lighter " + fontSize + "px Segoe UI";
+    var timeWidth = getTextWidth(hr.pad(2) + ":" + min.pad(2) + ":" + sec.pad(2) + ".000", font);
 
     document.getElementById("date").firstChild.nodeValue = months[month] + " " + day + ", " + year;
+    document.getElementById("time").style.marginLeft = (viewportWidth/2 - timeWidth/2) + "px";
     document.getElementById("time").firstChild.nodeValue = hr.pad(2) + ":" + min.pad(2) + ":" + sec.pad(2) + "." + ms;
 }
 
-function initClock() {
-    window.setInterval("updateClock()", 1);
+function init() {
+    window.setInterval(updateClock, 1);
 }
